9bec52cb7dea942195c262c5f57ee14368cdff9c
CarlosDanielks69 <92346886+CarlosDanielks69@users.noreply.github.com>
2026-04-24 00:50:56 -0300

    Calcular antecessor e sucessor de um valor

---
+++ 005.py
@@ -0,0 +1,10 @@
+n =int(input('Digite um número inteiro: '))
+a = n - 1
+s = n + 1
+print(f'O antecessor do numero {n} é: {a}')
+print(f'O sucessor do numero {n} é: {s}')
+
+#ou
+
+n1 = int(input('Digite um numero inteiro: '))
+print(f'O antecessor do numero {n1} é {n1-1}.\nO sucessor do numero {n1} é {n1+1}')
