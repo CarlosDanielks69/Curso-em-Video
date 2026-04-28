d3c218042ac6e3ec113cc4eb6d58b60b278823e9
Dani <92346886+CarlosDanielks69@users.noreply.github.com>
2026-04-28 02:21:19 -0300

    Update 018.py

--- 018.py
+++ 018.py
@@ -1,10 +1,8 @@
-import math
-angulo = float(input('Ângulo: '))
-radianos = math.radians(angulo)
-sen = math.sin(radianos)
-cos = math.cos(radianos)
-tan = math.tan(radianos)
-print(f'''
-O seno é: {sen:.2f}
-O coseno é {cos:.2f}
-A tangente é {tan:.2f}''')
+from math import radians, sin, cos, tan
+angulo = float(input('Digite o angulo que deseja: '))
+seno = sin(radians(angulo))
+print(f'O angulo de {angulo} tem o SENO de {seno:.2f}')
+cosseno = cos(radians(angulo))
+print(f'O angulo de {angulo} tem o COSSENO de {cosseno:.2f}')
+tangente = tan(radians(angulo))
+print(f'O angulo de {angulo} tem a TANGENTE de {tangente:.2f}')
