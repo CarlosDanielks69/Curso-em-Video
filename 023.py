f11b7a62ab4ded19c2bb330bfdb0844907402ec3
Dani <92346886+CarlosDanielks69@users.noreply.github.com>
2026-05-02 16:15:09 -0300

    Refactor number input handling and display logic

--- 023.py
+++ 023.py
@@ -1,6 +1,18 @@
-n = input('Digite um numer entre 0 e 9999: ')
-n1 = list(n)
-print(f'Unidade: {n1[-1]}')
-print(f'Dezena:  {n1[-2]}')
-print(f'Centena: {n1[-3]}')
-print(f'Milhar:  {n1[-4]}')
+n = int(input('Digite um numer entre 0 e 9999: '))
+n1 = str(n)
+print(f'Unidade: {n1[3]}')
+print(f'Dezena:  {n1[2]}')
+print(f'Centena: {n1[1]}')
+print(f'Milhar:  {n1[0]}')
+
+#ou
+
+num = int(input('Digite um numero entre 0 e 9999: '))
+u = num // 1 % 10
+d = num // 10 % 10
+c = num // 100 % 10
+m = num // 1000 % 10
+print(f'Unidade: {u}')
+print(f'Dezena: {d}')
+print(f'Centena: {c}')
+print(f'Milhar: {m}')
