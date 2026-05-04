8c3f305c9b8f6ac18dd35e72a79fd3e165fdda72
Dani <92346886+CarlosDanielks69@users.noreply.github.com>
2026-05-04 04:31:53 -0300

    Jogo da Adivinhação v.1.0

--- 028.py
+++ 028.py
@@ -1,6 +1,6 @@
 import random
 n = random.randint(1, 5)
-u = int(input('Digite um numero entre 1 e 5: '))
+u = int(input('Em que numero eu pensei? entre 1 e 5: '))
 if n == u:
     print(f'Você acertou!')
 else: print('Você errou!')
