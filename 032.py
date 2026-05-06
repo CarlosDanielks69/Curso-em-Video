bca098c9b7cbc4f3a8e3bb45124fb8b84a2a42b5
Dani <92346886+CarlosDanielks69@users.noreply.github.com>
2026-05-06 01:44:50 -0300

    Implement logic to determine the largest number
    Add a program to find the largest of three numbers.

---
+++ 032.py
@@ -0,0 +1,11 @@
+n1 = int(input('Digite o primeito numero: '))
+n2 = int(input('Digite o segundo numero: '))
+n3 = int(input('Digite o terceiro numero: '))
+if n1 > n2 and n1 > n3:
+    print(f'{n1} é o maior')
+if n3 > n1 and n3 > n2:
+    print(f'{n3} é o maior')
+if n2 > n1 and n2 > n3:
+    print(f'{n2} é o maior')
+elif:
+    print('Os numeros são iguais')
