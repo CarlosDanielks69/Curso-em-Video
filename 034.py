1667433a5728d35b5007661c128487a979064665
Dani <92346886+CarlosDanielks69@users.noreply.github.com>
2026-05-06 12:49:33 -0300

    Add salary increase calculation based on input

---
+++ 034.py
@@ -0,0 +1,10 @@
+s = float(input('Qual o seu salario? '))
+if s > 1250.00:
+    ns = s * 0.1 + s
+    a = 10
+else:
+    ns = s * 0.15 + s
+    a = 15
+print(f'''O seu salario teve um aumento de {a}%
+Antigo: R${s:.2f}
+Novo:   R${ns:.2f}''')
