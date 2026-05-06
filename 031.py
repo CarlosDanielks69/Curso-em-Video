705403241dfa59c31743be3ad89c0b3f0dc67d27
Dani <92346886+CarlosDanielks69@users.noreply.github.com>
2026-05-06 02:18:58 -0300

    Refactor price calculation and output format

--- 031.py
+++ 031.py
@@ -1,7 +1,6 @@
 n = float(input('Qual a distancia? '))
 if n < 200:
     d = n * 0.5
-    print(f'O preço da viagem será de {d}')
 else:
     d = n * 0.45
-    print(f'O preço da viagem será de {d}')
+print(f'O preço da viagem será de R${d:.2f}')
